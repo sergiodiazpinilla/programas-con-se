04d859dd1b41ba2f2148f042eb5782c744aeb334
sergiodiazpinilla <sergiodiaz.ciclos@gmail.com>
2015-11-12 13:39:58 +0100

    Archivos subido shur

---
+++ programación/01_printng/printing.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    printf("Zeeeeeeerpi <3.\n Dame"
+            :" amor pan. \n")
+    return EXIT_SUCCESS;
+}
---
+++ programación/03_trabajo1/03_trabajo1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    system("toilet -f border --gay -f pagga\"Holascott robertson")
+}
